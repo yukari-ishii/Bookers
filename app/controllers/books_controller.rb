6f33208a6800da85a774eec4d33466f11f8e5951
yukari-ishii <115003222+yukari-ishii@users.noreply.github.com>
2022-10-19 09:25:39 +0000

    [Finish]3-12テスト済

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.order(created_at: :desc)
+    @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -17,7 +22,25 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+
   #ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index' => 'books#index'
-  get 'books/show'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get root :to => 'homes#top'
+  
+  root to: 'homes#top'
+  resources :books
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
